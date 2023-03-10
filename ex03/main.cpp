b52453cec2043d90344615ce4688a06704638698
Phatcharaphol Suanprom <psuanpro@z3t4c1.42bangkok.com>
2023-03-10 23:37:31 +0700

    FIX main

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,15 +6,19 @@
 /*   By: psuanpro <Marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 23:25:08 by psuanpro          #+#    #+#             */
-/*   Updated: 2023/03/10 22:11:43 by psuanpro         ###   ########.fr       */
+/*   Updated: 2023/03/10 23:36:19 by psuanpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.hpp"
+#include "AMateria.hpp"
+#include "Cure.hpp"
 
 int main()
 {
 	IMateriaSource* src = new MateriaSource();
+	AMateria *nice = new Ice();
+	AMateria *ncure = new Cure();
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
 	ICharacter* me = new Character("me");
@@ -26,6 +30,8 @@ int main()
 	ICharacter* bob = new Character("bob");
 	me->use(0, *bob);
 	me->use(1, *bob);
+	delete nice;
+	delete ncure;
 	delete bob;
 	delete me;
 	delete src;
