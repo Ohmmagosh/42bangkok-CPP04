f9fc989bebee7cf55c68795a2d1d9ea7aba68f6f
Phatcharaphol Suanprom <psuanpro@z3t4c1.42bangkok.com>
2023-03-10 23:06:51 +0700

    FIX ex01 cat.cpp

--- ex01/Animal.hpp
+++ ex01/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: psuanpro <Marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 21:45:52 by psuanpro          #+#    #+#             */
-/*   Updated: 2023/03/08 09:58:03 by psuanpro         ###   ########.fr       */
+/*   Updated: 2023/03/10 22:39:30 by psuanpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,6 @@ class Animal {
 		virtual ~Animal();
 		std::string getType( void ) const;
 		virtual void makeSound( void ) const;
-
 };
 
 #endif
