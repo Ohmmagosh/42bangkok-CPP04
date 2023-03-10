5bc11b618ceeb5ed3c5a7490493e4218ea578754
Phatcharaphol Suanprom <psuanpro@z3t4c1.42bangkok.com>
2023-03-10 23:50:17 +0700

    Fix main ex03

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: psuanpro <Marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 23:25:08 by psuanpro          #+#    #+#             */
-/*   Updated: 2023/03/10 23:40:06 by psuanpro         ###   ########.fr       */
+/*   Updated: 2023/03/10 23:49:49 by psuanpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,10 @@ int main()
 	AMateria* tmp;
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
+	delete tmp;
 	tmp = src->createMateria("cure");
 	me->equip(tmp);
+	delete tmp;
 	ICharacter* bob = new Character("bob");
 	me->use(0, *bob);
 	me->use(1, *bob);
